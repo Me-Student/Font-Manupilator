574511c20597d607142f0bdccb8020c9dc88fc8e
Me-Student <84967182+Me-Student@users.noreply.github.com>
2021-08-19 17:46:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 function setup()
 {
-    canvas= createCanvas(300,300);
-    canvas.center();
+    canvas= createCanvas(500,500);
+    canvas.position(600,80);
+    video= createCapture(VIDEO);
+    video.size(550,500);
+
+    poseNet= ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log("PoseNet is loaded");
+}
+
+function draw()
+{
+    background("#F672CC");
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+    }
 }
